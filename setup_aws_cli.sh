4b3f8c9506c6301ffd3a297f38dca1523fe19727
Joni Harker <joni.harker@sagebase.org>
2026-05-20 11:35:42 -0700

    bugfix: fix travis errors
    The command `pip install cfn-lint sceptre sceptre-ssm-resolver`
    is failing on deep dependencies, move `awscli` install to the
    same pip command. Also move the pip install command before
    setup_aws_cli.sh so that it remains before configuring `awscli`,
    and upgrade Python from 3.12 to 3.13 to resolve deep dependencies
    on pyyaml from sceptre and awscli.

--- setup_aws_cli.sh
+++ setup_aws_cli.sh
@@ -11,7 +11,6 @@ if [[ (-z ${TRAVIS_TAG+x} || ${TRAVIS_TAG} = "") && ${TRAVIS_BRANCH} != "master"
 fi
 
 # setup the awscli with the correct aws account info.
-pip install awscli
 mkdir -p ~/.aws
 echo -e "[default]\nregion=us-east-1\nsource_profile=default\nrole_arn=$AwsCfServiceRoleArn" > ~/.aws/config
 echo -e "[default]\nregion=us-east-1\naws_access_key_id=$AwsTravisAccessKey\naws_secret_access_key=$AwsTravisSecretAccessKey" > ~/.aws/credentials
